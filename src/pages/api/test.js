5a7b32327e8d58949fc7fc3cf1f9aabf3e299481
Ytiti <cookieplus69@gmail.com>
2025-04-27 09:37:12 +0630

    File Phyat p

--- src/pages/api/test.js
+++
@@ -1,6 +0,0 @@
-export async function get() {
-  return new Response(JSON.stringify({ message: "Test API route working" }), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
-}
